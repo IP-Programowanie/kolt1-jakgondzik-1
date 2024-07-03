9d30950c83eef3374cee6ccd1d086af23bdd404f
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2024-07-03 16:37:44 +0200

    Finalna wersja

--- Kol2.cpp
+++ Kol2.cpp
@@ -17,35 +17,35 @@ enum class Stan
 class Samochod
 {
 protected:
-	char m_num_rej[9];
-	int m_licznik; // do 1km
+	char m_num_rej[9] = "";
+	int m_licznik = 0; // do 1km
 	Stan m_stan;
 	Wlasciciel m_posiadacz;
 	double m_war_pocz;
-	static Samochod* m_wzorcowa;
+	static Samochod * m_wzorcowa;
 public:
-	Samochod(const char* num_rej, int licznik, Stan stan, double m_war_pocz)
+	Samochod(const char* num_rej, int licznik, Stan stan,Wlasciciel posiadacz ,double m_war_pocz)
 	{
 		strcpy_s(m_num_rej, num_rej);
 		setLicznik(licznik);
 		m_stan = stan;
+		m_posiadacz = posiadacz;
+		setWarPocz(m_war_pocz);
 	}
 	Samochod()
 	{
 		if (m_wzorcowa == nullptr) { throw runtime_error("nie ma instancji wzorcowej"); }
-		strcpy_s(m_num_rej, m_wzorcowa->m_num_rej);	
+		strcpy_s(m_num_rej, m_wzorcowa->m_num_rej);
 		m_licznik = m_wzorcowa->m_licznik;
 		m_stan = m_wzorcowa->m_stan;
 		m_posiadacz = m_wzorcowa->m_posiadacz;
 		m_war_pocz = m_wzorcowa->m_war_pocz;
 
 	}
-
-	
 	void setLicznik(int licznik)
 	{
 		if (licznik < 0) { throw invalid_argument("Stan licznika jest ujemny"); }
-		m_licznik = lround(licznik); // zaokr¹glenie dodaj
+		m_licznik = lround(licznik);
 	}
 	int getLicznik()
 	{
@@ -56,16 +56,21 @@ public:
 		if(this->m_stan == Stan::niedopuszczony){ throw invalid_argument("Samochod nie jest dopuszczony do ruchu"); }
 		return 800;
 	}
-	static void setWzorcowa(Samochod * wzorzec)
-	{
-		m_wzorcowa = wzorzec;
-	}
 	void setWarPocz(double val)
 	{
 		if(val < 0){ throw invalid_argument("Auto nie moze miec ujemnej wartosci"); }
 		m_war_pocz = val;
 	}
-	operator double()
+
+	bool operator!=(const Samochod& s) const
+	{
+		return (abs(this->m_licznik - s.m_licznik) > 20 || (this->m_stan != s.m_stan));
+	}
+	static void setWzorcowa(Samochod* wzorzec)
+	{
+		m_wzorcowa = wzorzec;
+	}
+	double valCounter() // pomocnicza do liczenia wartosci
 	{
 		double val = (m_war_pocz - (0.2 * m_licznik));
 		if (m_stan == Stan::dopuszczony) { val = val * 1.0; }
@@ -73,9 +78,9 @@ public:
 		if (val < 400) { return 400; }
 		else { return val; }
 	}
-	bool operator!=(const Samochod& s) const
+	operator double()
 	{
-		return !(abs(this->m_licznik - s.m_licznik) > 20 && (this->m_stan != s.m_stan));
+		return valCounter();
 	}
 };
 class Samochod_elektryczny : public Samochod
@@ -83,7 +88,7 @@ class Samochod_elektryczny : public Samochod
 protected:
 	int m_bateria; //0-100
 public: 
-	Samochod_elektryczny(const char* num_rej, int licznik, Stan stan, double war_pocz, int bateria) : Samochod(num_rej, licznik, stan, war_pocz)
+	Samochod_elektryczny(const char* num_rej, int licznik, Stan stan, Wlasciciel posiadacz, double war_pocz, int bateria) : Samochod(num_rej, licznik, stan, posiadacz, war_pocz)
 	{
 		setBateria(bateria);
 	}
@@ -100,14 +105,13 @@ public:
 	}
 	operator double()
 	{
-		return m_war_pocz - (m_war_pocz * 0.3);
+		double wynik = valCounter();
+		return wynik - (wynik * 0.3);
 	}
 };
-
+Samochod* Samochod::m_wzorcowa = nullptr; //Wybor przez funkcje statyczn¹
 int main()
 {
-	Samochod * wzorzec = new Samochod("ABC12345", 10000, Stan::dopuszczony, 10000);
-	Samochod::setWzorcowa(wzorzec);
 }
 
 #endif
